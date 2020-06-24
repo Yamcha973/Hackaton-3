ab8bf1aebfea4f7d6806d796914504ff5f951d4a
Yamcha973 <61129914+Yamcha973@users.noreply.github.com>
2020-06-24 18:58:53 +0200

    second commit

--- server.js
+++ server.js
@@ -8,6 +8,18 @@ app.use(express.urlencoded({
     extended: true
 }));
 
+//Show all patients
+app.get('/api/patients', (req, res) => {
+    const patients = req.params.patients;
+    connection.query('SELECT * FROM patients p JOIN medecine_alert ma ON p.id=ma.patient_id WHERE ma.done=0', patients, (err, results) => {
+        if(err) {
+            res.status(500).send('Error while getting patient list');
+        } else {
+            res.json(results);
+        }
+    });
+})
+
 //Create a patient
 app.post('/api/patients', (req, res) => {
     const formData = req.body;
@@ -15,6 +27,32 @@ app.post('/api/patients', (req, res) => {
         if (err) {
             console.log(err);
             res.status(500).send('Error during patient creation');
+        } else {
+            res.sendStatus(200);
+        }
+    });
+});
+
+//Create a treatment for a patient
+app.post('/api/treatment/', (req, res) => {
+    const formData = req.body;
+    connection.query('INSERT INTO medecine_alert SET ?', formData, (err, results) => {
+        if (err) {
+            console.log(err);
+            res.status(500).send('Error during treatment creation');
+        } else {
+            res.sendStatus(200);
+        }
+    });
+});
+
+//Create a medicament
+app.post('/api/medicaments/', (req, res) => {
+    const formData = req.body;
+    connection.query('INSERT INTO medicaments SET ?', formData, (err, results) => {
+        if (err) {
+            console.log(err);
+            res.status(500).send('Error during medicament creation');
         } else {
             res.sendStatus(200);
         }
